c5cd4af2196a4cb301704c85ce5d19a61ac03514
Jessé Augusto <jessealvessilva@gmail.com>
2022-11-29 22:03:27 -0300

    Desmembramento do componente em partes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:intl/intl.dart';
-
-import '../models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+import '../componentes/transaction_list.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -56,49 +54,7 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Column(
-                children: _transaction.map((tr) {
-              return Card(
-                  child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 10,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'R\$ ${tr.value.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tr.title,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                ],
-              ));
-            }).toList()),
+            TransactionList(transaction: _transaction),
             Card(
               elevation: 5,
               child: Padding(
