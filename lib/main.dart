efd83de3e954a9ddd0c86e876a526b1614025ee7
Jesse <jessealvessilva@gmail.com>
2024-04-30 23:15:55 -0300

    Alteração do título/subtitulo dos itens

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyHomePage extends StatelessWidget {
     ),
     Transaction(
       id: "T2",
-      title: "Conta de Luz",
+      title: "Conta de Lu",
       value: 211.30,
       date: DateTime.now(),
     ),
@@ -48,11 +48,47 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Column(
-            children: <Widget>[
-              ..._transactions.map((tr) => Card(
-                    child: Text(tr.title),
-                  ))
-            ].toList(),
+            children: _transactions.map((tr) {
+              return Card(
+                  child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      tr.value.toString(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(tr.date.toString(),
+                          style: TextStyle(color: Colors.grey))
+                    ],
+                  )
+                ],
+              ));
+            }).toList(),
           )
         ],
       ),
